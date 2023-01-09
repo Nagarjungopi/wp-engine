96e25d7b24ccd8eeb0e24873a8c225b88b867e8f
Nagarjungopi <121079230+Nagarjungopi@users.noreply.github.com>
2023-01-09 12:59:20 +0530

    Delete plugins directory

--- plugins/gradient-button-for-elementor/modules/button/module.info.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-return [
-	'title' => __( 'Gradient Button', 'gradient-button-for-elementor' ),
-	'required' => true,
-	'default_activation' => true,
-];
